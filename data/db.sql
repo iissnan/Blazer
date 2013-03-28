923bf00090a0e8d58cce8900ccfa94f9fd68d1b0
IIssNan <iissnan@gmail.com>
2013-03-28 17:16:20 +0800

    Adjusted 	itle field to be an UNIQUE KEY

--- data/db.sql
+++ data/db.sql
@@ -1,23 +1,23 @@
 -- phpMyAdmin SQL Dump
 -- version 2.10.3
 -- http://www.phpmyadmin.net
---
+-- 
 -- 主机: localhost
--- 生成日期: 2013 年 03 月 28 日 06:21
+-- 生成日期: 2013 年 03 月 28 日 09:13
 -- 服务器版本: 5.0.51
 -- PHP 版本: 5.2.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
---
+-- 
 -- 数据库: `bookshelf`
---
+-- 
 
 -- --------------------------------------------------------
 
---
+-- 
 -- 表的结构 `books`
---
+-- 
 
 CREATE TABLE `books` (
   `id` int(11) NOT NULL auto_increment,
@@ -28,9 +28,11 @@ CREATE TABLE `books` (
   `cover` varchar(100) default NULL,
   `douban_link` varchar(100) default NULL,
   `create_at` datetime default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
 
---
+-- 
 -- 导出表中的数据 `books`
---
+-- 
+
